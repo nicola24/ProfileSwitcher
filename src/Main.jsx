8ebbf0aa8df19214f73f117cf31813e9016da020
Nicolas Peyrichou <nicolas.peyrichou@gmail.com>
2018-05-04 21:41:04 -0700

    styling with material-ui

--- src/Main.jsx
+++ src/Main.jsx
@@ -1,4 +1,9 @@
 import React, { Component } from 'react';
+import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
+import FloatingActionButton from 'material-ui/FloatingActionButton';
+import ChevronLeft from 'material-ui/svg-icons/navigation/chevron-left';
+import ChevronRight from 'material-ui/svg-icons/navigation/chevron-right';
+// import { blue500 } from 'material-ui/styles/colors';
 import axios from 'axios';
 
 import User from './User';
@@ -23,9 +28,23 @@ class Main extends Component {
 
   render() {
     return (
-      <div id="main">
-        {this.state.user.map(x => <User singleUser={x} key={x.id} />)}
-      </div>
+      <MuiThemeProvider>
+        <div>
+          <div>
+            <FloatingActionButton mini>
+              <ChevronLeft />
+            </FloatingActionButton>
+          </div>
+          <div>
+            {this.state.user.map(x => <User singleUser={x} key={x.id} />)}
+          </div>
+          <div>
+            <FloatingActionButton mini>
+              <ChevronRight />
+            </FloatingActionButton>
+          </div>
+        </div>
+      </MuiThemeProvider>
     );
   }
 }
--- src/User.jsx
+++ src/User.jsx
@@ -1,32 +1,67 @@
 import React from 'react';
+import {GridList, GridTile} from 'material-ui/GridList';
+import Paper from 'material-ui/Paper';
 import PropTypes from 'prop-types';
 
+const styles = {
+  root: {
+    display: 'flex',
+    flexWrap: 'wrap',
+    justifyContent: 'space-around',
+  },
+  gridList: {
+    width: 500,
+    height: 500,
+    overflowY: 'auto',
+  },
+  paper: {
+    height: 100,
+    width: 100,
+    margin: 20,
+    textAlign: 'center',
+    display: 'inline-block',
+  },
+};
+
 const User = ({ singleUser }) => (
-  <div>
-    <div id="profile_photo">
-      <img src="http://www.personalbrandingblog.com/wp-content/uploads/2017/08/blank-profile-picture-973460_640.png" alt="profile_photo" height="100" width="100" />
-    </div>
-    <div id="profile">
-      <h2>Profile</h2>
-      <p>{singleUser.name}</p>
-      <p>{singleUser.username}</p>
-      <p>{singleUser.email}</p>
-      <p>{singleUser.phone}</p>
-      <p>{singleUser.website}</p>
-    </div>
-    <div>
-      <h2 id="about_me">About Me</h2>
-      <p>{singleUser.company.name}</p>
-      <p>{singleUser.company.catchPhrase}</p>
-      <p>{singleUser.company.bs}</p>
-    </div>
-    <div id="address">
-      <h2>Address</h2>
-      <p>{singleUser.address.street}</p>
-      <p>{singleUser.address.suite}</p>
-      <p>{singleUser.address.city}</p>
-      <p>{singleUser.address.zipcode}</p>
-    </div>
+  <div style={styles.root}>
+    <GridList
+      cellHeight={250}
+      style={styles.gridList}
+    >
+      <GridTile className="animated slideInLeft">
+        <Paper style={styles.paper} zDepth={3} rounded={false}>
+          <img src="http://www.personalbrandingblog.com/wp-content/uploads/2017/08/blank-profile-picture-973460_640.png" alt="profile_photo" height="100" width="100" />
+        </Paper>
+      </GridTile>
+      <GridTile className="animated slideInLeft">
+        <Paper style={styles.paper} zDepth={3} rounded={false}>
+          <h2>Profile</h2>
+          <p>{singleUser.name}</p>
+          <p>{singleUser.username}</p>
+          <p>{singleUser.email}</p>
+          <p>{singleUser.phone}</p>
+          <p>{singleUser.website}</p>
+        </Paper>
+      </GridTile>
+      <GridTile className="animated slideInRight">
+        <Paper style={styles.paper} zDepth={3} rounded={false}>
+          <h2>About Me</h2>
+          <p>{singleUser.company.name}</p>
+          <p>{singleUser.company.catchPhrase}</p>
+          <p>{singleUser.company.bs}</p>
+        </Paper>
+      </GridTile>
+      <GridTile className="animated slideInRight">
+        <Paper style={styles.paper} zDepth={3} rounded={false}>
+          <h2>Address</h2>
+          <p>{singleUser.address.street}</p>
+          <p>{singleUser.address.suite}</p>
+          <p>{singleUser.address.city}</p>
+          <p>{singleUser.address.zipcode}</p>
+        </Paper>
+      </GridTile>
+    </GridList>
   </div>
 );
 
@@ -36,3 +71,59 @@ User.propTypes = {
 };
 
 export default User;
+
+
+// import React from 'react';
+// import {GridList, GridTile} from 'material-ui/GridList';
+// import IconButton from 'material-ui/IconButton';
+// import Subheader from 'material-ui/Subheader';
+// import StarBorder from 'material-ui/svg-icons/toggle/star-border';
+//
+// const styles = {
+//   root: {
+//     display: 'flex',
+//     flexWrap: 'wrap',
+//     justifyContent: 'space-around',
+//   },
+//   gridList: {
+//     width: 500,
+//     height: 450,
+//     overflowY: 'auto',
+//   },
+// };
+//
+// const tilesData = [
+//   {
+//     img: 'images/grid-list/00-52-29-429_640.jpg',
+//     title: 'Breakfast',
+//     author: 'jill111',
+//   },
+//   {
+//     img: 'images/grid-list/burger-827309_640.jpg',
+//     title: 'Tasty burger',
+//     author: 'pashminu',
+//   },
+// ];
+//
+// const GridListExampleSimple = () => (
+//   <div style={styles.root}>
+//     <GridList
+//       cellHeight={180}
+//       style={styles.gridList}
+//     >
+//       <Subheader>December</Subheader>
+//       {tilesData.map((tile) => (
+//         <GridTile
+//           key={tile.img}
+//           title={tile.title}
+//           subtitle={<span>by <b>{tile.author}</b></span>}
+//           actionIcon={<IconButton><StarBorder color="white" /></IconButton>}
+//         >
+//           <img src={tile.img} />
+//         </GridTile>
+//       ))}
+//     </GridList>
+//   </div>
+// );
+//
+// export default GridListExampleSimple;
