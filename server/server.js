ab1aa6da20a180399a06d55eb03cf62a23c1f2fc
Nicolas Peyrichou <nicolas.peyrichou@gmail.com>
2018-05-08 09:39:39 -0700

    deploy heroku

--- server/server.js
+++ server/server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const request = require('request');
 
 const app = express();
-const port = process.env.PORT || 3000;
+const port = 3000;
 
 app.use(express.static('dist'));
 
